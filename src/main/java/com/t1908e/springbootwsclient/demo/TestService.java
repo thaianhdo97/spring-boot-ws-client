83d56b8114ae79296805f4cbf97bb844d9c3d891
thaianh97 <58674701+thaianh97@users.noreply.github.com>
2021-07-16 21:24:44 +0700

    upload project

--- src/main/java/com/t1908e/springbootwsclient/demo/TestService.java
+++ src/main/java/com/t1908e/springbootwsclient/demo/TestService.java
@@ -1,12 +1,7 @@
 package com.t1908e.springbootwsclient.demo;
 
-import com.example.springbootwsclient.service.HelloService;
-import com.example.springbootwsclient.service.HelloServiceService;
-
 public class TestService {
     public static void main(String[] args) {
-        HelloServiceService hss = new HelloServiceService();
-        HelloService hs = hss.getHelloServicePort();
-        System.out.println();
+
     }
 }
